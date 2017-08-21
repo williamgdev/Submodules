041ce5553b81aec479d9c4992ee96c19986f8664
William Gutierrez <williamg.dev@gmail.com>
2017-08-21 11:58:17 -0500

    Fixed the Libraries name folder, Added the dependency and Pulled new changes from submodule remote

--- app/src/main/java/com/github/williamgdev/example/submodules/MainActivity.java
+++ app/src/main/java/com/github/williamgdev/example/submodules/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-//import com.github.williamgdev.example.submodules.restapi.RestAPI;
-//import com.github.williamgdev.example.submodules.restapi.RestAPIListener;
+import com.github.williamgdev.example.submodules.restapi.RestAPI;
+import com.github.williamgdev.example.submodules.restapi.RestAPIListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        RestAPI.getValue(new RestAPIListener<Integer>() {
-//            @Override
-//            public void onResult(Integer result) {
-//
-//                Log.d(TAG, "onCreate: RestCall value = " + result);
-//            }
-//        });
+        RestAPI.getValue(new RestAPIListener<Integer>() {
+            @Override
+            public void onResult(Integer result) {
+
+                Log.d(TAG, "onCreate: RestCall value = " + result);
+            }
+        });
     }
 }
